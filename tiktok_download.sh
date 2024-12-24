0cb591db7b5410d2656e4a560b4c17635a5d8d8e
WFC-29 <cyberwfc29@gmail.com>
2024-12-24 22:25:31 +0700

    tiktok_download.sh
    Menambahkan file script utama untuk mendownload video TikTok tanpa watermark.

--- tiktok_download.sh
+++ tiktok_download.sh
@@ -1,12 +1,23 @@
-#! /bin/bash
-pkg update && pkg upgrade
-pkg install bash
-pkg install python
-pkg install ffmpeg
-pkg install git
-pkg install toilet
-pip install yt-dlp
-termux-setup-storage
-git clone https://github.com/WFC-29/Tiktok-Downloader.git
-chmod +x tiktok_download.sh
-./tiktok_download.sh
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Menampilkan ASCII art "CYBER WFC29" dengan toilet
+clear
+toilet -f mono12 -F border CYBER
+toilet -f mono12 -F border WFC29
+echo "Download Video TikTok tanpa watermark"
+
+# Meminta input link TikTok
+echo "Masukkan link video TikTok:"
+read link
+
+# Mendefinisikan direktori tujuan penyimpanan
+download_dir="$HOME/storage/shared/DCIM/Camera"
+                                                                       # Memastikan direktori tujuan ada
+if [ ! -d "$download_dir" ]; then
+    echo "Direktori tujuan tidak ditemukan. Pastikan folder 'Camera' a>
+    exit 1
+fi                                                                     
+# Menggunakan yt-dlp untuk mendownload video tanpa watermark dan menyi>yt-dlp --no-warnings --quiet --remove-chapters --no-post-overwrites -->
+
+# Menampilkan pesan setelah video selesai diunduh
+echo "Video berhasil didownload ke $download_dir!"
